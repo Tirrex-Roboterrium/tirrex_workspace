0147377722353ac662c39297b0cce2d807fbfa28
Cyrille Pierre <cyrille.pierre@inrae.fr>
2023-11-03 15:11:02 +0100

    fix ros_setup.sh: source correct workspace

--- docker/ros_setup.sh
+++ docker/ros_setup.sh
@@ -2,7 +2,7 @@
 set -e
 source /usr/share/gazebo/setup.sh
 source /opt/ros/$ROS_DISTRO/setup.bash
-source ~/projects/tirrex_ws/install/local_setup.bash || true
+source $WORKSPACE/install/local_setup.bash || true
 export RCUTILS_COLORIZED_OUTPUT=1
 export RCUTILS_CONSOLE_OUTPUT_FORMAT="[{severity}] {name}: {message}"
 exec $@
