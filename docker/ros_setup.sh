4f6cdff3fd172867950b2ce6ce45a92615b0282b
Cyrille Pierre <cyrille.pierre@inrae.fr>
2024-02-21 15:32:21 +0100

    Add auto-completion in bash dockerservice

--- docker/ros_setup.sh
+++ docker/ros_setup.sh
@@ -3,6 +3,8 @@ set -e
 source /usr/share/gazebo/setup.sh
 source /opt/ros/$ROS_DISTRO/setup.bash
 source $WORKSPACE/install/local_setup.bash || true
+
 export RCUTILS_COLORIZED_OUTPUT=1
 export RCUTILS_CONSOLE_OUTPUT_FORMAT="[{severity}] {name}: {message}"
+
 exec $@
