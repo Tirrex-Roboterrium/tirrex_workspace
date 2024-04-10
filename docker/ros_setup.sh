5c9271f22742142e57ac18bfe06c78931476d8c1
Cyrille Pierre <cyrille.pierre@inrae.fr>
2024-04-10 10:52:40 +0200

    Fix GAZEBO_MODEL_PATH and 'repositories' file

--- docker/ros_setup.sh
+++ docker/ros_setup.sh
@@ -6,5 +6,7 @@ source $WORKSPACE/install/local_setup.bash || true
 
 export RCUTILS_COLORIZED_OUTPUT=1
 export RCUTILS_CONSOLE_OUTPUT_FORMAT="[{severity}] {name}: {message}"
+export GAZEBO_RESOURCE_PATH="$WORKSPACE/gazebo:$GAZEBO_RESOURCE_PATH"
+export GAZEBO_MODEL_PATH="$WORKSPACE/gazebo/models:$GAZEBO_MODEL_PATH"
 
 exec $@
