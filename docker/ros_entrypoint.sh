4df6b35d584ac894b49e5acd24b01804e688b042
Cyrille Pierre <cyrille.pierre@inrae.fr>
2025-08-01 16:50:58 +0200

    Dockerfile: Add env in builder + restore ros_entrypoint

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 set -e
 
-# override TIRREX_WORKSPACE if it is the full docker image
-if [[ -n "$TIRREX_IMAGE_FULL" ]] ; then
-  export TIRREX_WORKSPACE='/opt/tirrex_ws'
-fi
-
 if [[ -z "$WORKSPACE" ]] ; then 
   echo >&2 "Missing environment variable 'WORKSPACE'"
   exit 1
 fi
-cd -- "$WORKSPACE"
 
 if [[ -z "$USER" ]] ; then 
   echo >&2 "Missing environment variable 'USER'"
   exit 2
 fi
+
+cd -- "$WORKSPACE"
 
 # execute command as USER
 exec sudo -snEHu "$USER" -- /ros_setup.sh $@
--- docker/ros_setup.sh
+++ docker/ros_setup.sh
@@ -4,14 +4,18 @@ source /usr/share/gazebo/setup.sh
 source /opt/ros/$ROS_DISTRO/setup.bash
 
 # The TIRREX_WORKSPACE variable is only defined in workspaces that extend this one
-if [[ -n "$TIRREX_WORKSPACE" ]] ; then
+if [[ -n "$TIRREX_WORKSPACE" && "$TIRREX_WORKSPACE" != "$WORKSPACE" ]] ; then
   if [[ -r "$TIRREX_WORKSPACE/install/local_setup.bash" ]] ; then
     source "$TIRREX_WORKSPACE/install/local_setup.bash"
   else
-    echo >&2 "Error: env TIRREX_WORKSPACE is defined but is not compiled."
-    echo >&2 "You have to go in this directory and build it:"
-    echo >&2 "  cd $TIRREX_WORKSPACE"
-    echo >&2 "  docker compose run --rm compile"
+    if [[ -e "$TIRREX_WORKSPACE" ]] ; then
+      echo >&2 "Error: env TIRREX_WORKSPACE is defined but is not compiled."
+      echo >&2 "You have to go in this directory and build it:"
+      echo >&2 "  cd $TIRREX_WORKSPACE"
+      echo >&2 "  docker compose run --rm compile"
+    else
+      echo >&2 "Error: Workspace '$TIRREX_WORKSPACE' does not exist."
+    fi
     exit 1
   fi
 
