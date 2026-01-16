1b95dc23dfeabc321a4fda7daa3774ca51869259
Cyrille Pierre <cyrille.pierre@inrae.fr>
2026-01-16 12:58:26 +0100

    entrypoint that works with all container tools

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -8,15 +8,24 @@ fi
 
 cd -- "$WORKSPACE"
 
+ws_uid=$(stat -c "%u" "$WORKSPACE")
+
 # execute command as USER if it is the owner of $WORKSPACE
-if [[ $(id -u) = 0 && $(stat -c "%u" "$WORKSPACE") != 0 ]] ; then
-  if [[ -z "$USER" ]] ; then 
-    echo >&2 "Missing environment variable 'USER'"
+if [[ $(id -u) = 0 && "$ws_uid" != 0 ]] ; then
+  if [[ -z "$USER" && -z "$HOME" ]] ; then 
+    echo >&2 "Missing environment variable 'USER' or 'HOME'"
     exit 2
   fi
   
-  exec sudo -snEHu "$USER" -- /ros_setup.sh $@
+  # create user if it does not exist
+  if ! id -u "$USER" &>/dev/null ; then
+    ws_gid=$(stat -c "%g" "$WORKSPACE")
+    groupadd -g "$ws_gid" "$USER"
+    useradd -u "$ws_uid" -g "$ws_gid" -s /bin/bash -d "$HOME" -G dialout "$USER"
+  fi
+
+  exec sudo -snEHu "$USER" -- /ros_setup.sh "$@"
 
 else
-  exec /ros_setup.sh $@
+  exec /ros_setup.sh "$@"
 fi
--- docker/ros_setup.sh
+++ docker/ros_setup.sh
@@ -8,14 +8,7 @@ if [[ -n "$TIRREX_WORKSPACE" && "$TIRREX_WORKSPACE" != "$WORKSPACE" ]] ; then
   if [[ -r "$TIRREX_WORKSPACE/install/local_setup.bash" ]] ; then
     source "$TIRREX_WORKSPACE/install/local_setup.bash"
   else
-    if [[ -e "$TIRREX_WORKSPACE" ]] ; then
-      echo >&2 "Error: env TIRREX_WORKSPACE is defined but is not compiled."
-      echo >&2 "You have to go in this directory and build it:"
-      echo >&2 "  cd $TIRREX_WORKSPACE"
-      echo >&2 "  docker compose run --rm compile"
-    else
-      echo >&2 "Error: Workspace '$TIRREX_WORKSPACE' does not exist."
-    fi
+    echo >&2 "Error: Workspace '$TIRREX_WORKSPACE' does not exist."
     exit 1
   fi
 
@@ -34,4 +27,5 @@ export RCUTILS_CONSOLE_OUTPUT_FORMAT="[{severity}] {name}: {message}"
 export GAZEBO_RESOURCE_PATH="$WORKSPACE/gazebo:$GAZEBO_RESOURCE_PATH"
 export GAZEBO_MODEL_PATH="$WORKSPACE/gazebo/models:$GAZEBO_MODEL_PATH"
 
-exec $@
+# use 'bash -c' to interpret variables in the given command line
+exec bash -c "exec $*"
